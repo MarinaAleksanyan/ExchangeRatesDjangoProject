ded20dcb3310f4acb82254c4b4a22bbe48560a15
MarinaAleksanyan <marina.aleksanyann@gmail.com>
2023-01-31 20:48:29 +0400

    [UPDT] Add new function (branches)

--- mysite/rates/views.py
+++ mysite/rates/views.py
@@ -5,18 +5,25 @@ from .models import Bank, BankBranches
 
 def index(request):
     bank = Bank.objects.all()
+    branch = BankBranches.objects.all()
 
     context = {
         'bank': bank,
+        'branch': branch,
         'title': 'Rates.am',
     }
 
     return render(request, template_name="ratesPages/index.html", context=context)
 
 
-def branches(request):
-    branch = BankBranches.objects.all()
+def branches(request, bank_id):
+    branch = BankBranches.objects.filter(bank_id=bank_id)
+    banks = Bank.objects.all()
+    bank = Bank.objects.get(pk=bank_id)
     context = {
-        'branch': branch
+        'branch': branch,
+        'bank': bank,
+        'banks': banks,
+        'title': 'Rates.am',
     }
     return render(request, template_name="ratesPages/branches.html", context=context)
