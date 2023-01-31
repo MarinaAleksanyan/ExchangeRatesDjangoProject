c51a8e37615257efa927b41f8fc9df85a0394e08
MarinaAleksanyan <marina.aleksanyann@gmail.com>
2023-01-31 20:48:56 +0400

    [UPDT] Add new path for branches

--- mysite/rates/urls.py
+++ mysite/rates/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', index),
-    path('branches/', branches)
+    path('', index, name='home'),
+    path('bank/<int:bank_id>/', branches, name='branches'),
 ]
